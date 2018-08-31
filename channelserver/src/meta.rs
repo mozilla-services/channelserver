25bae0ccb7aa2a9228fedf914c459e52f43bf2b9
jrconlin <me+crypt@jrconlin.com>
2018-08-30 17:59:50 -0700

    f refactored meta for eventual location testing

--- channelserver/src/meta.rs
+++ channelserver/src/meta.rs
@@ -57,9 +57,7 @@ fn get_preferred_language_element(
     for lang in langs {
         // It's a wildcard, so just return the first possible choice.
         if lang == "*" {
-            return elements.values().into_iter().next().map(|e|
-                e.to_owned()
-            )
+            return elements.values().into_iter().next().map(|e| e.to_owned());
         }
         if elements.contains_key(lang) {
             if let Some(element) = elements.get(lang.as_str()) {
@@ -78,38 +76,167 @@ fn get_preferred_language_element(
     None
 }
 
-fn handle_city_err(log: &Addr<logging::MozLogger>, err: &MaxMindDBError) {
+fn handle_city_err(log: Option<&Addr<logging::MozLogger>>, err: &MaxMindDBError) {
     match err {
-        maxminddb::MaxMindDBError::InvalidDatabaseError(s) =>
-            log.do_send(logging::LogMessage{
+        maxminddb::MaxMindDBError::InvalidDatabaseError(s) => log.map(|l| {
+            l.do_send(logging::LogMessage {
                 level: logging::ErrorLevel::Critical,
-                msg: format!("Invalid GeoIP database! {:?}", s)
-            }),
-        maxminddb::MaxMindDBError::IoError(s) => 
-            log.do_send(logging::LogMessage{
-                level: logging::ErrorLevel::Critical,
-                msg: format!("Could not read from database file! {:?}", s)
-            }),
-        maxminddb::MaxMindDBError::MapError(s) =>
-            log.do_send(logging::LogMessage{
-                level: logging::ErrorLevel::Warn,
-                msg: format!("Mapping error: {:?}", s)
-            }),
-        maxminddb::MaxMindDBError::DecodingError(s) =>
-            log.do_send(logging::LogMessage{
-                level: logging::ErrorLevel::Warn,
-                msg: format!("Could not decode mapping result: {:?}", s)
-            }),
-        maxminddb::MaxMindDBError::AddressNotFoundError(s) =>
-            log.do_send(logging::LogMessage{
-                level: logging::ErrorLevel::Debug,
-                msg: format!("Could not find address for IP: {:?}", s)
-            }),
-        _ => 
-            log.do_send(logging::LogMessage{
-                level: logging::ErrorLevel::Error,
-                msg: format!("Unknown GeoIP error encountered: {:?}", err)
+                msg: format!("Invalid GeoIP database! {:?}", s),
             })
+        }),
+        maxminddb::MaxMindDBError::IoError(s) => log.map(|l| {
+            l.do_send(logging::LogMessage {
+                level: logging::ErrorLevel::Critical,
+                msg: format!("Could not read from database file! {:?}", s),
+            })
+        }),
+        maxminddb::MaxMindDBError::MapError(s) => log.map(|l| {
+            l.do_send(logging::LogMessage {
+                level: logging::ErrorLevel::Warn,
+                msg: format!("Mapping error: {:?}", s),
+            })
+        }),
+        maxminddb::MaxMindDBError::DecodingError(s) => log.map(|l| {
+            l.do_send(logging::LogMessage {
+                level: logging::ErrorLevel::Warn,
+                msg: format!("Could not decode mapping result: {:?}", s),
+            })
+        }),
+        maxminddb::MaxMindDBError::AddressNotFoundError(s) => log.map(|l| {
+            l.do_send(logging::LogMessage {
+                level: logging::ErrorLevel::Debug,
+                msg: format!("Could not find address for IP: {:?}", s),
+            })
+        }),
+        _ => log.map(|l| {
+            l.do_send(logging::LogMessage {
+                level: logging::ErrorLevel::Error,
+                msg: format!("Unknown GeoIP error encountered: {:?}", err),
+            })
+        }),
+    };
+}
+
+fn get_ua(headers: &http::HeaderMap, log: Option<&Addr<logging::MozLogger>>) -> Option<String> {
+    if let Some(ua) = headers
+        .get(http::header::USER_AGENT)
+        .map(|s| match s.to_str() {
+            Err(x) => {
+                log.map(|l| {
+                    l.do_send(logging::LogMessage {
+                        level: logging::ErrorLevel::Warn,
+                        msg: format!("Bad UA string: {:?}", x),
+                    })
+                });
+                // We have to return Some value here.
+                return "".to_owned();
+            }
+            Ok(s) => s.to_owned(),
+        }) {
+        if ua == "".to_owned() {
+            // If it's blank, it's None.
+            return None;
+        }
+        return Some(ua);
+    }
+    None
+}
+
+fn get_location(
+    sender: &mut SenderData,
+    langs: &Vec<String>,
+    log: Option<&Addr<logging::MozLogger>>,
+    iploc: &maxminddb::Reader,
+) {
+    if sender.remote.is_some() {
+        log.map(|l| {
+            l.do_send(logging::LogMessage {
+                level: logging::ErrorLevel::Debug,
+                msg: format!("Looking up IP: {:?}", sender.remote),
+            })
+        });
+        // Strip the port from the remote (if present)
+        let remote = sender
+            .remote
+            .clone()
+            .map(|mut r| {
+                let end = r.find(':').unwrap_or(r.len());
+                r.drain(..end).collect()
+            })
+            .unwrap_or(String::from(""));
+        if let Ok(loc) = remote.parse() {
+            if let Ok(city) = iploc.lookup::<City>(loc).map_err(|err| {
+                handle_city_err(log, &err);
+                err
+            }) {
+                /*
+                    The structure of the returned maxminddb record is:
+                    City:maxminddb::geoip::model::City {
+                        city: Some(City{
+                            geoname_id: Some(#),
+                            names: Some({"lang": "name", ...})
+                            }),
+                        continent: Some(Continent{
+                            geoname_id: Some(#),
+                            names: Some({...})
+                            }),
+                        country: Some(Country{
+                            geoname_id: Some(#),
+                            names: Some({...})
+                            }),
+                        location: Some(Location{
+                            latitude: Some(#.#),
+                            longitude: Some(#.#),
+                            metro_code: Some(#),
+                            time_zone: Some(".."),
+                            }),
+                        postal: Some(Postal {
+                            code: Some("..")
+                            }),
+                        registered_country: Some(Country {
+                            geoname_id: Some(#),
+                            iso_code: Some(".."),
+                            names: Some({"lang": "name", ...})
+                            }),
+                        represented_country: None,
+                        subdivisions: Some([Subdivision {
+                            geoname_id: Some(#),
+                            iso_code: Some(".."),
+                            names: Some({"lang": "name", ...})
+                            }]),
+                        traits: None }
+                    }
+                */
+                if let Some(names) = city
+                    .city
+                    .and_then(|c: maxminddb::geoip2::model::City| c.names)
+                {
+                    sender.city = get_preferred_language_element(&langs, names);
+                }
+                if let Some(names) = city
+                    .country
+                    .and_then(|c: maxminddb::geoip2::model::Country| c.names)
+                {
+                    sender.country = get_preferred_language_element(&langs, names);
+                }
+                // because consistency is overrated.
+                for subdivision in city.subdivisions {
+                    if let Some(subdivision) = subdivision.get(0) {
+                        if let Some(names) = subdivision.clone().names {
+                            sender.region = get_preferred_language_element(&langs, names);
+                            break;
+                        }
+                    }
+                }
+            } else {
+                log.map(|l| {
+                    l.do_send(logging::LogMessage {
+                        level: logging::ErrorLevel::Info,
+                        msg: format!("No location info for IP: {:?}", sender.remote),
+                    })
+                });
+            }
+        }
     }
 }
 
@@ -135,112 +262,20 @@ impl From<HttpRequest<WsChannelSessionState>> for SenderData {
                 preferred_languages(lang.to_owned())
             }
         };
-        let conn = req.connection_info();
         // parse user-header for platform info
-        sender.ua = match headers.get(http::header::USER_AGENT) {
-            None => None,
-            Some(s) => match s.to_str() {
-                Err(x) => {
-                    log.do_send(logging::LogMessage {
-                        level: logging::ErrorLevel::Warn,
-                        msg: format!("Bad UA string: {:?}", x),
-                    });
-                    None
-                }
-                Ok(s) => Some(s.to_owned()),
-            },
-        };
-        sender.remote = conn.remote().map(|r| r.to_owned());
-        if sender.remote.is_some() {
-            log.do_send(logging::LogMessage {
-                        level: logging::ErrorLevel::Debug,
-                        msg: format!("Looking up IP: {:?}", sender.remote),
-                    });
-            // Strip the port from the remote (if present)
-            let remote = sender.remote.clone().map(|mut r| {
-                    let end = r.find(':').unwrap_or(r.len());
-                    r.drain(..end).collect()
-            }).unwrap_or(String::from(""));
-            if let Ok(loc) = remote.parse() {
-                if let Ok(city) = req.state().iploc.lookup::<City>(loc).map_err(|err| {
-                    handle_city_err(&log, &err);
-                    err
-                }) {
-                    /*
-                        The structure of the returned maxminddb record is:
-                        City:maxminddb::geoip::model::City {
-                            city: Some(City{
-                                geoname_id: Some(#),
-                                names: Some({"lang": "name", ...})
-                                }),
-                            continent: Some(Continent{
-                                geoname_id: Some(#),
-                                names: Some({...})
-                                }),
-                            country: Some(Country{
-                                geoname_id: Some(#),
-                                names: Some({...})
-                                }),
-                            location: Some(Location{
-                                latitude: Some(#.#),
-                                longitude: Some(#.#),
-                                metro_code: Some(#),
-                                time_zone: Some(".."),
-                                }),
-                            postal: Some(Postal {
-                                code: Some("..")
-                                }),
-                            registered_country: Some(Country {
-                                geoname_id: Some(#),
-                                iso_code: Some(".."),
-                                names: Some({"lang": "name", ...})
-                                }),
-                            represented_country: None,
-                            subdivisions: Some([Subdivision {
-                                geoname_id: Some(#),
-                                iso_code: Some(".."),
-                                names: Some({"lang": "name", ...})
-                                }]),
-                            traits: None }
-                        }
-                    */
-                    if let Some(names) = city
-                        .city
-                        .and_then(|c: maxminddb::geoip2::model::City| c.names)
-                    {
-                        sender.city = get_preferred_language_element(&langs, names);
-                    }
-                    if let Some(names) = city
-                        .country
-                        .and_then(|c: maxminddb::geoip2::model::Country| c.names)
-                    {
-                        sender.country = get_preferred_language_element(&langs, names);
-                    }
-                    // because consistency is overrated.
-                    for subdivision in city.subdivisions {
-                        if let Some(subdivision) = subdivision.get(0) {
-                            if let Some(names) = subdivision.clone().names {
-                                sender.region = get_preferred_language_element(&langs, names);
-                                break;
-                            }
-                        }
-                    }
-                } else {
-                    log.do_send(logging::LogMessage {
-                        level: logging::ErrorLevel::Info,
-                        msg: format!("No location info for IP: {:?}", sender.remote),
-                    });
-                }
-            }
-        }
+        sender.ua = get_ua(&headers, Some(&log));
+        sender.remote = req.connection_info().remote().map(|rem| rem.to_owned());
+        get_location(&mut sender, &langs, Some(&log), &req.state().iploc);
         sender
     }
 }
 
 #[cfg(test)]
 mod test {
-    use super::{get_preferred_language_element, preferred_languages};
+    use super::*;
     use std::collections::BTreeMap;
+    
+    use http;
 
     #[test]
     fn test_preferred_language() {
@@ -289,5 +324,30 @@ mod test {
             None,
             get_preferred_language_element(&goof_lang, elements.clone())
         );
+    }
+
+    #[test]
+    fn test_ua() {
+        let good_header = "Mozilla/5.0 Foo";
+        let blank_header = "";
+        let mut good_headers = http::HeaderMap::new();
+        good_headers.insert(http::header::USER_AGENT, http::header::HeaderValue::from_static(good_header));
+        assert_eq!(Some(good_header.to_owned()), get_ua(&good_headers, None));
+        let mut blank_headers = http::HeaderMap::new();
+        blank_headers.insert(http::header::USER_AGENT, http::header::HeaderValue::from_static(blank_header));
+        assert_eq!(None, get_ua(&blank_headers, None));
+        let empty_headers = http::HeaderMap::new();
+        assert_eq!(None, get_ua(&empty_headers, None));
+    }
+
+    #[ignore]
+    #[test]
+    fn test_location() {
+        let test_ip = "63.245.208.195"; // Mozilla
+
+        let langs = vec!["en".to_owned()];
+        let mut sender = SenderData::default();
+        sender.remote = Some(test_ip.to_owned());
+        // TODO: either mock maxminddb::Reader or pass it in as a wrapped impl
     }
 }
