6da70cd4b4bd3035697ce419db422cff00a97ddb
jrconlin <jr+git@mozilla.com>
2024-03-21 16:57:42 -0700

    chore: Update for 2024-03
    * Switch to thiserror
    * remove old tokio dependencies (and audits)

--- src/error.rs
+++ src/error.rs
@@ -1,63 +1,49 @@
 use std::fmt;
 use std::io;
 
-use failure::{Backtrace, Context, Fail};
+use backtrace::Backtrace;
+use thiserror::Error;
 
 #[derive(Debug)]
 pub struct HandlerError {
-    inner: Context<HandlerErrorKind>,
-}
-
-#[derive(Clone, Eq, PartialEq, Debug, Fail)]
-pub enum HandlerErrorKind {
-    #[fail(display = "Excess Data Exchanged: {:?}", _0)]
-    XSDataErr(String),
-    #[fail(display = "Excess Messages: {:?}", _0)]
-    XSMessageErr(String),
-    #[fail(display = "IO Error: {:?}", _0)]
-    IOError(String),
-    #[fail(display = "Could not start metrics: {:?}", _0)]
-    MetricsError(String),
-    #[fail(display = "Bad remote address: {:?}", _0)]
-    BadRemoteAddrError(String),
-}
-
-impl Fail for HandlerError {
-    fn cause(&self) -> Option<&dyn Fail> {
-        self.inner.cause()
-    }
-
-    fn backtrace(&self) -> Option<&Backtrace> {
-        self.inner.backtrace()
-    }
+    pub kind: HandlerErrorKind,
+    pub backtrace: Box<Backtrace>,
 }
 
 impl fmt::Display for HandlerError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        fmt::Display::fmt(&self.inner, f)
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.kind.fmt(f)
     }
 }
 
-impl From<HandlerErrorKind> for HandlerError {
-    fn from(kind: HandlerErrorKind) -> HandlerError {
-        Context::new(kind).into()
+impl std::error::Error for HandlerError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        self.kind.source()
     }
 }
 
-impl From<Context<HandlerErrorKind>> for HandlerError {
-    fn from(inner: Context<HandlerErrorKind>) -> HandlerError {
-        HandlerError { inner }
+impl<T> From<T> for HandlerError
+where
+    HandlerErrorKind: From<T>,
+{
+    fn from(item: T) -> Self {
+        HandlerError {
+            kind: HandlerErrorKind::from(item),
+            backtrace: Box::new(Backtrace::new()),
+        }
     }
 }
 
-impl From<io::Error> for HandlerError {
-    fn from(err: io::Error) -> HandlerError {
-        Context::new(HandlerErrorKind::IOError(format!("{:?}", err))).into()
-    }
-}
-
-impl From<cadence::MetricError> for HandlerError {
-    fn from(err: cadence::MetricError) -> HandlerError {
-        Context::new(HandlerErrorKind::MetricsError(format!("{:?}", err))).into()
-    }
+#[derive(Debug, Error)]
+pub enum HandlerErrorKind {
+    #[error("Excess Data Exchanged: {:?}", _0)]
+    XSDataErr(String),
+    #[error("Excess Messages: {:?}", _0)]
+    XSMessageErr(String),
+    #[error(transparent)]
+    IOError(#[from] io::Error),
+    #[error(transparent)]
+    MetricsError(#[from] cadence::MetricError),
+    #[error("Bad remote address: {:?}", _0)]
+    BadRemoteAddrError(String),
 }
